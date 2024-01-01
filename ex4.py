09d4da404cd584c88796c7b703f542606f563ece
joykepler <joyjoykepler@gmail.com>
2024-01-01 18:51:42 -0300

    ex4

--- ex4.py
+++ ex4.py
@@ -3,7 +3,7 @@ bim2 = float(input('Insira a nota do segundo bimestre '))
 bim3 = float(input('Insira a nota do terceiro bimestre '))
 bim4 = float(input('Insira a nota do quarto bimestre '))
 
-resultado = bim1 + bim2 + bim3 + bim4
+resultado = (bim1 + bim2 + bim3 + bim4) / 4
 
 print('A média é', resultado)
 
