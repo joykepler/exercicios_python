cd08c0230998ab8b07b566ce5aa3ce2fe2537ea2
joykepler <joyjoykepler@gmail.com>
2024-01-01 19:47:49 -0300

    area do quadrado

---
+++ ex7.py
@@ -0,0 +1,8 @@
+lado1 = float(input('Insira o valor de um lado do quadrado '))
+lado2 = float(input('Insira o valor do outro lado do quadrado '))
+
+area = lado1 * lado2
+
+resultado = area * 2
+
+print('O dobro da área do quadrado é', resultado)
