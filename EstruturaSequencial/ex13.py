95780015f0a7d259fac008eef7fa8185b3e22bf2
joykepler <joyjoykepler@gmail.com>
2024-01-03 19:41:04 -0300

    python

---
+++ EstruturaSequencial/ex13.py
@@ -0,0 +1,8 @@
+altura = float(input('Insira sua altura '))
+
+peso_homem =   (72.7 * altura) - 58
+
+peso_mulher =  (62.1 * altura) - 44.7
+
+print('Seu peso ideal é, se for homem:', peso_homem)
+print('Se for mulher:', peso_mulher)
---
+++ EstruturaSequencial/ex14.py
@@ -0,0 +1,9 @@
+peso = float(input('Quantos quilos de peixe? '))
+
+limite = 50
+
+excesso = peso - limite
+
+multa = excesso * 4
+
+print('Valor da multa: R$',multa)
